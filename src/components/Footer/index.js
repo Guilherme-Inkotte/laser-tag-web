24e3664deb5d241c16f33df04d891c63c8fc1f1b
Guilherme Inkotte <gui.inkotte@hotmail.com>
2020-05-31 17:24:30 -0300

    Adding footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import './styles.css';
+import styles from './styles.module.css';
 
 function Footer() {
-  return <div />;
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footerContent}>
+        <div className={styles.leftSide}>
+          <div className={styles.listSection}>
+            <p className={styles.listLabel}>Membros</p>
+            <ul className={styles.list}>
+              <li>Arthur Teixeira</li>
+              <li>Guilherme Inkotte</li>
+              <li>Isabela Milena</li>
+              <li>Lucas Timoteo</li>
+              <li>Luíza Klein</li>
+              <li>Matheus Maicá</li>
+            </ul>
+          </div>
+          <div className={styles.listSection}>
+            <p className={styles.listLabel}>Recursos</p>
+            <ul className={styles.list}>
+              <li><a target="blank" href="https://github.com/Guilherme-Inkotte/laser-tag">Github.com/laser-tag</a></li>
+              <li><a target="blank" href="https://github.com/Guilherme-Inkotte/laser-tag-web">Github.com/laser-tag-web</a></li>
+              <li><a target="blank" href="https://github.com/Guilherme-Inkotte/laser-tag-api">Github.com/laser-tag-api</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.rightSide}>
+          <h3>LOGO</h3>
+          <p>&copy;Lasertag</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Footer from '../components/Footer'
 
 function Home() {
-  return <h1>Home</h1>;
+  return <Footer />;
 }
 
 export default Home;
